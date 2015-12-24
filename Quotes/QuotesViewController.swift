9ed896a08ad511a88f8bb760ef3fdd79b11ea2ca
少常 <zhencheng.zc@alipay.com>
2015-12-24 16:20:36 +0800

    the quotes and quit the app

--- Quotes/QuotesViewController.swift
+++ Quotes/QuotesViewController.swift
@@ -9,10 +9,43 @@
 import Cocoa
 
 class QuotesViewController: NSViewController {
+    // MARK: Properties
+    
+    @IBOutlet var textLabel: NSTextField!
+    let quotes = Quote.all
+    var currentQuoteIndex: Int = 0 {
+        didSet {
+            updateQuote()
+        }
+    }
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        currentQuoteIndex = 0
+    }
+    
+    func updateQuote() {
+        textLabel.stringValue = String(quotes[currentQuoteIndex])
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
     }
     
+}
+// MARK: Actions
+
+extension QuotesViewController {
+    @IBAction func goLeft(sender: NSButton) {
+        currentQuoteIndex = (currentQuoteIndex - 1 + quotes.count) % quotes.count
+    }
+    
+    @IBAction func goRight(sender: NSButton) {
+        currentQuoteIndex = (currentQuoteIndex + 1) % quotes.count
+    }
+    
+    @IBAction func quit(sender: NSButton) {
+        NSApplication.sharedApplication().terminate(sender)
+    }
 }
