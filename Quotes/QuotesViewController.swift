22089be71cf61c86bf8b1e52a2685141799dca28
少常 <zhencheng.zc@alipay.com>
2015-12-24 14:48:04 +0800

    use menu

---
+++ Quotes/QuotesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuotesViewController.swift
+//  Quotes
+//
+//  Created by shaochang on 15/12/24.
+//  Copyright © 2015年 shaochang. All rights reserved.
+//
+
+import Cocoa
+
+class QuotesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
